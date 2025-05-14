7080db8d47e3127f755d56784f55632c217c19b5
hamza97benroummane <hamza.benroummane@gmail.com>
2025-05-14 13:31:45 +0100

    update token

--- src/components/AppWalletProvider.tsx
+++ src/components/AppWalletProvider.tsx
@@ -1,3 +1,54 @@
+// ////////////////////////////////////////////////// DEVNET ///////////////////////////////////////
+
+// // src/components/AppWalletProvider.tsx
+// import React, { FC, useMemo } from 'react';
+// import { clusterApiUrl } from '@solana/web3.js';
+// import {
+//   ConnectionProvider,
+//   WalletProvider,
+// } from '@solana/wallet-adapter-react';
+// import {
+//   WalletModalProvider,
+// } from '@solana/wallet-adapter-react-ui';
+// import {
+//   PhantomWalletAdapter,
+//   SolflareWalletAdapter,
+//   TorusWalletAdapter,
+// } from '@solana/wallet-adapter-wallets';
+// import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+
+// import '@solana/wallet-adapter-react-ui/styles.css';
+
+// export const AppWalletProvider: FC<{ children: React.ReactNode }> = ({ children }) => {
+//   const network = WalletAdapterNetwork.Devnet;
+//   const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+//   const wallets = useMemo(
+//     () => [
+//       new PhantomWalletAdapter(),
+//       new SolflareWalletAdapter(),
+//       new TorusWalletAdapter(),
+//     ],
+//     []
+//   );
+
+//   return (
+//     <ConnectionProvider endpoint={endpoint}>
+//       <WalletProvider wallets={wallets} autoConnect>
+//         <WalletModalProvider>{children}</WalletModalProvider>
+//       </WalletProvider>
+//     </ConnectionProvider>
+//   );
+// };
+
+
+
+
+
+
+
+
+
 import React, { FC, useMemo, useCallback } from 'react';
 import { clusterApiUrl } from '@solana/web3.js';
 import {
